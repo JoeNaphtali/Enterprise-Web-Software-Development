44b7a9adcb7cd3ec492ddc4ce3892d77717afe76
paulchibamba <61985521+paulchibamba@users.noreply.github.com>
2020-04-02 05:46:41 +0200

    Posts from database displaying on home page

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -13,6 +13,8 @@
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
 		<!-- JQuery -->
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 		<!-- Bootstrap tooltips -->
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
 		<!-- Bootstrap core JavaScript -->
--- includes/ideas.col.inc.php
+++ includes/ideas.col.inc.php
@@ -1,61 +1,57 @@
-<div class="col-md-8">
-    <div class="row">
-        <div class="col-sm-6">
-            <h1 class="my-4 latest-ideas">Latest Ideas</h1>
-        </div>
-        <div class="col-sm-6">
-            <a href="../../Enterprise-Web-Software-Development/propose.php"><button class="btn btn-propose my-4">Propose an Idea</button></a>
-        </div>
-    </div>
-    <!-- Idea -->
-
-    <div class="card mb-4">
-        <div class="card-body">
-            <h2 class="card-title">Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-            <a href="post/post.php">Read More &rarr;</a>
-        </div>
-        <div>
-            <div class="card-footer text-muted">
-                Posted on March 30, 2020 by <a href="#">A. Dumbass Nigga</a>
+    <div class="col-md-8">
+        <div class="row">
+            <div class="col-sm-6">
+                <h1 class="my-4 latest-ideas">Latest Ideas</h1>
+            </div>
+            <div class="col-sm-6">
+                <a href="../../Enterprise-Web-Software-Development/propose.php"><button class="btn btn-propose my-4">Propose an Idea</button></a>
             </div>
         </div>
-    </div>
+        <!-- Retriving Data From DB-->
+        <?php 
+    $query = "SELECT * FROM idea";
+    $select_all_query = mysqli_query($conn, $query);
+    
+    while($row = mysqli_fetch_assoc($select_all_query)){
+        $idea_title = $row['idea_title'];
+        $idea_content = $row['content'];
+        $idea_date = $row['post_date'];
+        $idea_user = $row['post_user'];
+        $idea_thumbs_up = $row['upvote_count'];
+        $idea_thumbs_down = $row['downvote_count'];
+        
+        ?>
 
-    <!-- /.Idea -->
+        <!--Displaying data from database-->
+        <!-- Idea -->
 
+        <div class="card mb-4">
+            <div class="card-body">
+                <h2 class="card-title"><?php echo "$idea_title"; ?></h2>
+                <p class="card-text"><?php echo "$idea_content"; ?></p>
+                <a href="post/post.php">Read More &rarr;</a>
+            </div>
+            <div>
+                <div class="card-footer text-muted">
+                    Posted on <?php echo "$idea_date"; ?> by <a href="#"><?php echo "$idea_user"; ?></a>
+                    <i class="fas fa-thumbs-up"> <?php echo "$idea_thumbs_up"; ?></i>
+                    <i class="fas fa-thumbs-down"> <?php echo "$idea_thumbs_down"; ?></i>
+                </div>
+            </div>
+        </div>
 
+        <!-- /.Idea -->
 
-    <!-- Idea -->
+        <!-- Closing While loop -->
+        <?php } ?>
 
-    <!--
-					<div class="card mb-4">
-						<div class="card-body">
-							<h2 class="card-title">Title</h2>
-							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-							<a href="post/post.php">Read More &rarr;</a>
-						</div>
-						<div>
-							<div class="card-footer text-muted">
-								Posted on March 30, 2020 by <a href="#">A. Dumbass Nigga</a>
-							</div>
-						</div>
-					</div>
--->
+        <!-- Pagination -->
 
-    <!-- Idea -->
-
-
-
-
-
-    <!-- Pagination -->
-
-    <ul class="pagination justify-content-center mb-4">
-        <li class="page-item">
-            <a class="page-link" href="#">&larr; Older</a>
-        </li>
-        <li class="page-item disabled">
-            <a class="page-link" href="#">Newer &rarr;</a>
-        </li>
-    </ul>
+        <ul class="pagination justify-content-center mb-4">
+            <li class="page-item">
+                <a class="page-link" href="#">&larr; Older</a>
+            </li>
+            <li class="page-item disabled">
+                <a class="page-link" href="#">Newer &rarr;</a>
+            </li>
+        </ul>
